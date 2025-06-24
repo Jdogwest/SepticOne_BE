027f4488c0a2415f20f5a99c3ec9ca5064fcbbc7
Konstantin Yozhikov <131611574+Jdogwest@users.noreply.github.com>
2025-06-24 19:13:53 +0300

    Update router.py

--- app/requests/router.py
+++ app/requests/router.py
@@ -25,12 +25,12 @@ async def get_my_requests(user_data: User = Depends(get_current_user)):
     return await RequestDAO.find_my_requests(user_data.id)
 
 
-@router.get('/{id}', summary="Получить заявку по id")
+@router.get('/{id}/', summary="Получить заявку по id")
 async def get_request_by_id(id: int, user_data: User = Depends(get_current_manager_user)):
     return await RequestDAO.find_request_by_id(id)
 
 
-@router.put("/edit/{id}", summary="Редактировать заявку")
+@router.put("/edit/{id}/", summary="Редактировать заявку")
 async def edit_request(id: int, data: SRequestEdit, user_data: User = Depends(get_current_manager_user)):
     return await RequestDAO.edit_request(id, data)
 
@@ -40,6 +40,6 @@ async def get_busy_dates():
     return await RequestDAO.find_busy_dates()
 
 
-@router.get('/brigades_on_date/{date}', summary="Получить бригады на определенную дату")
+@router.get('/brigades_on_date/{date}/', summary="Получить бригады на определенную дату")
 async def get_brigades_on_date(date: date, user_data: User = Depends(get_current_manager_user)):
     return await RequestDAO.find_brigades_on_date(date)
